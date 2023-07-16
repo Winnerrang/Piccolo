1a0cec3759576851e56dbf17f68bded54288de96
Winnerrang <leo.hanxu@mail.utoronto.ca>
2023-07-15 22:17:43 -0400

    Add comment for public_singleton.h

--- engine/source/runtime/core/base/public_singleton.h
+++ engine/source/runtime/core/base/public_singleton.h
@@ -9,15 +9,21 @@ namespace Pilot
     class PublicSingleton
     {
     protected:
+        //tell the compiler to generate the default constructor
         PublicSingleton() = default;
 
     public:
+        // does not allow getInstance to throw exceptions if T is nothrow constructible
         static T& getInstance() noexcept(std::is_nothrow_constructible<T>::value)
         {
             static T instance;
             return instance;
         }
+
+        //The destructor can not throw any exception and it is telling the compiler to generate the default destructor
         virtual ~PublicSingleton() noexcept = default;
+
+        //tell the compiler not to generate the copy constructor and the copy assignment operator
         PublicSingleton(const PublicSingleton&) = delete;
         PublicSingleton& operator=(const PublicSingleton&) = delete;
     };
